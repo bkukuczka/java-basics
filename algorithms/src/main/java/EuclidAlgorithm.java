5b8b6d405233b138dc0a2e15a4e02cc03a421248
kuki <kukuczkabartlomiej@gmail.com>
2019-08-25 12:22:59 +0200

    [ADD] Euclid algorithm

---
+++ algorithms/src/main/java/EuclidAlgorithm.java
@@ -0,0 +1,16 @@
+public class EuclidAlgorithm {
+    public static int findBiggestCommonDivisor(int numA, int numB) {
+        if (numA == 0 || numB == 0) {
+            return 0;
+        }
+
+        return bgc(numA, numB);
+    }
+
+    private static int bgc(int numA, int numB) {
+        if (numB == 0) {
+            return numA;
+        }
+        return bgc(numB, numA % numB);
+    }
+
---
+++ algorithms/src/test/java/EuclidAlgorithmTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EuclidAlgorithmTest {
+
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+                Arguments.of(33, 11, 11),
+                Arguments.of(24, 88, 8),
+                Arguments.of(0, 39, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void findBiggestCommonDivisor(int numberOne, int numberTwo, int expectedResult) {
+        //when
+        int result = EuclidAlgorithm.findBiggestCommonDivisor(numberOne, numberTwo);
+
+        //then
+        assertEquals(expectedResult, result);
+    }
+}
